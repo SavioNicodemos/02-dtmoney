6e20661588699c471c6475b751a43f23fbe9f788
SavioNicodemos <saviogomes_182@hotmail.com>
2021-11-03 15:44:37 -0300

    Finished Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,8 @@ export const GlobalStyle = createGlobalStyle`
     --background: #F0F2F5;
     --shape: #FFFFFF;
 
-    --red : #E52E4D;
+    --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
     --blue-light: #6933FF;
 
